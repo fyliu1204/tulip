50db4f82f552621b479c5f5cf3183b98ed627a98
fyliu1204 <fyliu@umich.edu>
2017-04-28 02:43:51 -0400

    Algo1 completed

--- original_bisimulation.py
+++ original_bisimulation.py
@@ -49,5 +49,90 @@ def bisimulation_algorithm(fts):
                     c_list.append(list(set(si).difference(set(pre_sj))))
                     break
     new_fts = trs.FTS()
-    #still working on it
+    new_fts.name = fts.name
+    for state in c_list:
+        a = fts.states.find([state[0]])
+        (s0_, label) = a[0]
+        new_fts.states.add(''.join(state))
+        for ap in label['ap']:
+            new_fts.atomic_propositions.add(ap)
+        new_fts.states[''.join(state)]['ap'] = label['ap']
+    for sys_actions in fts.sys_actions:
+        new_fts.sys_actions.add(sys_actions)
+    for env_actions in fts.env_actions:
+        new_fts.env_actions.add(env_actions)
+    for ini_state in fts.states.initial:
+        if ini_state in new_fts.states:
+            new_fts.states.initial.add(ini_state)
+        else:
+            for state in new_fts.states:
+                if ini_state in state:
+                    new_fts.states.initial.add(state)
+                    break
+    for tran in fts.transitions(data=True):
+        s1 = ''
+        s2 = ''
+        if tran[0] in new_fts.states:
+            s1 = tran[0]
+        else:
+            for state in new_fts.states:
+                if tran[0] in state:
+                    s1 = state
+                    break
+        if tran[1] in new_fts.states:
+            s2 = tran[1]
+        else:
+            for state in new_fts.states:
+                if tran[1] in state:
+                    s2 = state
+                    break
+        flag = False
+        for tmp in new_fts.transitions():
+            if s1 == tmp[0] and s2 == tmp[1]:
+                flag = True
+                break
+        if flag:
+            continue
+        new_fts.transitions.add(
+            s1, s2, 
+            sys_actions = tran[2]['sys_actions'],
+            env_actions = tran[2]['env_actions']
+        )
+    print new_fts
     return new_fts
+
+ofts = trs.FiniteTransitionSystem()
+
+ofts.states.add_from(['s1', 's2', 's3'] )
+ofts.states.initial.add('s1')
+
+ofts.atomic_propositions |= ['p','p1']
+apset = set()
+apset.add('p1')
+apset.add('p')
+ofts.states['s1']['ap'] = apset
+ofts.states.add('s2', ap=set() )
+ofts.states.add('s3', ap={'p','p1'})
+
+ofts.transitions.add('s1', 's2') # unlabeled
+
+ofts.sys_actions.add('try')
+ofts.sys_actions.add_from({'start', 'stop'} )
+ofts.env_actions.add_from({'block', 'wait'} )
+
+# remove unlabeled edge s1->s2
+ofts.transitions.remove('s1', 's2')
+
+ofts.transitions.add(
+    's1', 's2',
+    sys_actions='try', env_actions='block'
+)
+ofts.transitions.add(
+    's2', 's3',
+    sys_actions='start', env_actions='wait'
+)
+ofts.transitions.add(
+    's3', 's2',
+    sys_actions='stop', env_actions='block'
+)
+fts = bisimulation_algorithm(ofts)
